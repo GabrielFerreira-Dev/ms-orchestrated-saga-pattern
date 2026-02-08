5b28c45c20dedb377bec13c10d8b0eefcaf41997
Gabriel <gabrieel.ferreira55@gmail.com>
2026-02-08 19:30:23 -0300

    starter configuration

---
+++ orchestrator-service/orchestrator-service/src/test/java/com/github/orchestrator_service/OrchestratorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.orchestrator_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestratorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
